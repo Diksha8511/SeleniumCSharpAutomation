be060d745417b35b842ae1320d1a4ddbddfe0b19
Diksha8511 <diksha85119@gmail.com>
2022-04-25 22:35:54 +0530

    Automation testing using selenium

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumCsharpAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //reference for a webdriver specific to browser
+            IWebDriver driver = new ChromeDriver();
+
+            //pass the url
+            driver.Url = "https://app.cloudqa.io/home/AutomationPracticeForm";
+
+            var inputFields = driver.FindElements(By.TagName("input"));
+
+            IWebElement firstInputElement = inputFields[0];
+            firstInputElement.SendKeys("Diksha");
+
+            IWebElement secondInputElement = inputFields[1];
+            secondInputElement.SendKeys("Pandit" + Keys.Tab);
+
+            IWebElement thirdInputElement = inputFields[3];
+            System.Threading.Thread.Sleep(2000);
+            thirdInputElement.Click();
+            thirdInputElement.SendKeys(Keys.Tab);
+
+        }
+    }
+}
